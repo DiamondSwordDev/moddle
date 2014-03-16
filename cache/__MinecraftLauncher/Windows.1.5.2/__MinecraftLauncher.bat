582c9d7796eb09d40fd1d5dd306eaa08046148b4
Nathan2055 <spychicken2055@gmail.com>
2014-03-16 15:47:35 -0400

    Delete all of the old code to prepare for the Grand Java Rewrite
    Signed-off-by: Nathan2055 <spychicken2055@gmail.com>

--- cache/__MinecraftLauncher/Windows.1.5.2/__MinecraftLauncher.bat
+++
@@ -1,4 +0,0 @@
-CD /d "{APPDATA}.minecraft\"
-SET APPDATA={APPDATA}
-JAVA -Xms{MEMORY}m -Xmx{MEMORY}m -cp "%APPDATA%\.minecraft\bin\minecraft.jar;%APPDATA%\.minecraft\bin\jinput.jar;%APPDATA%\.minecraft\bin\lwjgl.jar;%APPDATA%\.minecraft\bin\lwjgl_util.jar" -Djava.library.path="%APPDATA%\.minecraft\bin\natives" net.minecraft.client.Minecraft {USERNAME} {SESSIONID}
-PAUSE
--- moddle-wingui/README.md
+++
@@ -1,4 +0,0 @@
-Moddle Windows Front-end
-=============
-
-The C# front-end for Moddle. This is only intended for use during alpha, and will be replaced with a Java front-end before the final release.
