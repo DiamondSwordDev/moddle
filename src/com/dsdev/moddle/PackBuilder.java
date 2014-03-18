69e6af449a28235b46d82ced5743a958f7cceb0a
Nathan2055 <spychicken2055@gmail.com>
2014-03-17 21:29:19 -0400

    Add the new NetBeans project

---
+++ src/com/dsdev/moddle/PackBuilder.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dsdev.moddle;
+
+/**
+ *
+ * @author Greenlock28
+ */
+public class PackBuilder {
+    
+    private String ModpackName = "";
+    
+    public PackBuilder(String name) {
+        ModpackName = name;
+    }
+    
+    
+    
+    public void buildPack() {
+        
+        
+        
+    }
+    
+    public void runPack() {
+        
+    }
+    
+}
