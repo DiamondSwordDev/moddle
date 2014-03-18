5d1c2f7854354e80b17cec83fee816a5ce549ef1
Nathan2055 <spychicken2055@gmail.com>
2014-03-18 15:08:27 -0400

    Miscellaneous cleanup
    I moved the libraries into their own folder and added their licenses. I also added some general Javadoc to the PackBuilder class.
    Signed-off-by:Nathan2055 <spychicken2055@gmail.com>

--- src/com/dsdev/moddle/PackBuilder.java
+++ src/com/dsdev/moddle/PackBuilder.java
@@ -1,40 +1,38 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.dsdev.moddle;
 
 import java.io.File;
 import org.apache.commons.io.FileUtils;
 
 /**
+ * The mod pack compiler.
  *
  * @author Greenlock28
  */
 public class PackBuilder {
-    
+
     private String ModpackName = "";
-    
+
     public PackBuilder(String name) {
         ModpackName = name;
     }
-    
-    
+
     public void buildPack() {
         try {
-        
+
             System.out.println("[Build] Creating pack directory...");
             File modpackDir = new File("./packs/" + ModpackName);
-            if (!modpackDir.exists())
+            if (!modpackDir.exists()) {
                 FileUtils.forceMkdir(modpackDir);
+            }
 
             File dotMinecraft = new File("./packs/" + ModpackName + "/.minecraft");
-            if (!dotMinecraft.exists())
+            if (!dotMinecraft.exists()) {
                 FileUtils.forceMkdir(dotMinecraft);
-        
+            }
+
         } catch (Exception ex) {
             System.out.println("[Build][ERROR] " + ex.getMessage());
         }
     }
-    
+
 }
