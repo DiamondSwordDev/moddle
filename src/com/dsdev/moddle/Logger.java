465c02c5de6cf37d99178378e618fccf05d9986f
Greenlock <pokedoxysm@gmail.com>
2014-03-21 23:06:26 -0400

    Recommitted today's changes
    Re-added decompressZipfile()
    Re-added getFile()
    Wrote some more builder code

--- src/com/dsdev/moddle/Logger.java
+++ src/com/dsdev/moddle/Logger.java
@@ -17,8 +17,24 @@ public class Logger {
         System.out.println("[" + getCurrentTimeStamp() + "][Moddle][Info] " + message);
     }
     
+    public static void info(String prefix, String message) {
+        System.out.println("[" + getCurrentTimeStamp() + "][Moddle][" + prefix + "][Info] " + message);
+    }
+    
+    public static void warning(String message) {
+        System.out.println("[" + getCurrentTimeStamp() + "][Moddle][Warning] " + message);
+    }
+    
+    public static void warning(String prefix, String message) {
+        System.out.println("[" + getCurrentTimeStamp() + "][Moddle][" + prefix + "][Warning] " + message);
+    }
+    
     public static void error(String message) {
         System.out.println("[" + getCurrentTimeStamp() + "][Moddle][Error] " + message);
+    }
+    
+    public static void error(String prefix, String message) {
+        System.out.println("[" + getCurrentTimeStamp() + "][Moddle][" + prefix + "][Error] " + message);
     }
     
     
