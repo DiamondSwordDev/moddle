dba4b538683255d1acad53aac07cc2ee48db50ae
Greenlock <pokedoxysm@gmail.com>
2014-03-25 19:45:50 -0400

    Trying to roll a download method.
    A beginning attempt at getting a Commons IO download method written.
    (One does not simply download a file in Commons IO?)

--- src/com/dsdev/moddle/PackBuilder.java
+++ src/com/dsdev/moddle/PackBuilder.java
@@ -1,16 +1,20 @@
 package com.dsdev.moddle;
 
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
+import sun.net.ProgressListener;
 
 /**
  * The mod pack compiler.
@@ -46,7 +50,9 @@ public class PackBuilder {
             JSONObject packConfig = (JSONObject)JSONValue.parse(FileUtils.readFileToString(getFile("./tmp/pack/pack.json")));
             
             Logger.info("Loading version config...");
-            JSONObject versionConfig = (JSONObject)JSONValue.parse(FileUtils.readFileToString(getFile("./data/versions/" + packConfig.get("version") + ".json")));
+            JSONObject versionConfig = (JSONObject)JSONValue.parse(FileUtils.readFileToString(getFile("./data/" + packConfig.get("version") + ".json")));
+            
+            JSONArray libraryList
             
         } catch (Exception ex) {
             Logger.error(ex.getMessage());
@@ -77,6 +83,38 @@ public class PackBuilder {
         } catch (Exception ex) {
             Logger.error("UnZip", ex.getMessage());
             return false;
+        }
+    }
+    
+    public boolean downloadFile(String webAddress, String targetFile) {
+        String x = null;
+        OutputStream os = null;
+        InputStream is = null;
+        ProgressListener progressListener = new ActionListener();
+        try {
+            os = new FileOutputStream(getFile(targetFile));
+            is = (new URL(webAddress)).openStream();
+
+            DownloadCountingOutputStream dcount = new DownloadCountingOutputStream(os);
+            dcount.setListener(progressListener);
+
+            // this line give you the total length of source stream as a String.
+            // you may want to convert to integer and store this value to
+            // calculate percentage of the progression.
+            dl.openConnection().getHeaderField("Content-Length");
+
+            // begin transfer by writing to dcount, not os.
+            IOUtils.copy(is, dcount);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (os != null) { 
+                os.close(); 
+            }
+            if (is != null) { 
+                is.close(); 
+            }
         }
     }
     
