55984dbf0df56e3af2ccdf6cab336ebfe314f114
Greenlock <pokedoxysm@gmail.com>
2014-07-23 08:40:38 -0400

    Initial commit for MUpdate project

---
+++ mupdate/src/com/dsdev/mupdate/resources/Resources.java
@@ -0,0 +1,31 @@
+
+package com.dsdev.mupdate.resources;
+
+import java.io.IOException;
+import javax.swing.ImageIcon;
+import org.apache.commons.io.IOUtils;
+
+/**
+ *
+ * @author Diamond Sword Development
+ */
+public class Resources {
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // This class is mainly a placeholder to allow access to the resource     //
+    // images in this package.                                                //
+    ////////////////////////////////////////////////////////////////////////////
+    
+    public static ImageIcon getImageResource(String name) {
+        return new ImageIcon(Resources.class.getResource(name));
+    }
+    
+    public static String getTextResource(String name) {
+        try {
+            return IOUtils.toString(Resources.class.getResourceAsStream(name), "UTF-8");
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+    
+}
